a67d1e3e6da11576500aa3818f8691d793612cb3
Piotr Snopek <piotr.snopek@hotmail.com>
2023-02-23 19:23:53 +0100

    init vue

--- README.md
+++
@@ -1,2 +0,0 @@
-1. npm install
-2. npm install vue@next vue-loader@next @vitejs/plugin-vue
---
+++ resources/js/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+    import { reactive, ref, computed, watch, watchEffect } from 'vue';
+</script>
+
+<template>
+    <h1> Hello, Vuejs with Laravel </h1>
+</template>
+
